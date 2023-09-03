bba9bf96747a0ab1fc33da2895270fd3d4a67c8a
merricat <zkerr4032@outlook.com>
2023-09-03 10:27:33 +0100

    Change desktop names

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "TERM", "WEB", "CHAT", "MEDIA", "MUSIC", "ETC" };
+static const char *tags[] = { "一", "二", "三", "四", "五", "六" };
 
 static const Rule rules[] = {
 	/* xprop(1):
